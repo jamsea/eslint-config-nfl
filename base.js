583207e49c12dd19de5153b1617a4b4687ca4c8f
Richard Herrera <richard.herrera@nfl.com>
2016-04-18 15:46:27 -0700

    feat: Port NFL lint rules to AirBnb-style linter setup

--- base.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: [
-        "eslint-config-nfl/legacy",
-        "eslint-config-nfl/rules/es6"
-    ],
-    rules: {}
-};
--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: [
-        "eslint-config-nfl/base",
-        "eslint-config-nfl/rules/strict",
-        "eslint-config-nfl/rules/react",
-        "eslint-config-nfl/rules/mocha"
-    ],
-    rules: {}
-};
