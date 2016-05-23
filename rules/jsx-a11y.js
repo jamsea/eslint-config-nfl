52d84ca40b86cb9d62f122566b91775c5c1b6475
James Hush <james.hush@nfl.com>
2016-05-23 14:15:01 -0700

    chore(eslint): Upgrade eslint-plugin-jsx-a11y
    Upgrade eslint-plugin-jsx-a11y and add new eslint rules

--- rules/jsx-a11y.js
+++ rules/jsx-a11y.js
@@ -14,20 +14,26 @@ module.exports = {
 
         // Require <img> to have a non-empty `alt` prop, or role="presentation"
         // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/img-uses-alt.md
-        "jsx-a11y/img-uses-alt": [1, ["Image", "Thumbnail"]],
+        "jsx-a11y/img-has-alt": [1, ["Image", "Thumbnail"]],
 
         // Prevent img alt text from containing redundant words like "image", "picture", or "photo"
         // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/redundant-alt.md
-        "jsx-a11y/redundant-alt": 1,
+        "jsx-a11y/img-redundant-alt": 1,
 
         // Require ARIA roles to be valid and non-abstract
         // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/valid-aria-role.md
-        "jsx-a11y/valid-aria-role": 2,
-
-        "jsx-a11y/onclick-uses-role": 1,
-        "jsx-a11y/mouse-events-map-to-key-events": 1,
-        "jsx-a11y/use-onblur-not-onchange": 0,
-        "jsx-a11y/label-uses-for": [1, "label"],
-        "jsx-a11y/no-hash-href": 1
+        "jsx-a11y/aria-role": 2,
+        "jsx-a11y/mouse-events-have-key-events": 1,
+        "jsx-a11y/label-has-for": [1, "label"],
+        "jsx-a11y/href-no-hash": 1,
+        "jsx-a11y/aria-props": 1,
+        "jsx-a11y/aria-proptypes": 1,
+        "jsx-a11y/aria-unsupported-elements": 1,
+        "jsx-a11y/no-onchange": 0,
+        "jsx-a11y/onclick-has-focus": 0,
+        "jsx-a11y/onclick-has-role": 1,
+        "jsx-a11y/role-has-required-aria-props": 1,
+        "jsx-a11y/role-supports-aria-props": 1,
+        "jsx-a11y/tabindex-no-positive": 1
     }
 };
